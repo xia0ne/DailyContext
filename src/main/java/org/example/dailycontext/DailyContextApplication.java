938f8a8fda7654318b1ef1e199a5d9c13ab4e567
xia0ne <1223605525@qq.com>
2023-12-24 20:34:52 +0800

    init

---
+++ src/main/java/org/example/dailycontext/DailyContextApplication.java
@@ -0,0 +1,13 @@
+package org.example.dailycontext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyContextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/dailycontext/DailyContextApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dailycontext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyContextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
